14801bac8425b0ddb66419c78e1500e1359e03e7
Benjamin Espinoza <beespinoza2022@inf.udec.cl>
2023-10-17 11:23:15 -0300

    feat: se agrego la interfaz comparable en Moneda

--- src/main/java/org/example/Moneda.java
+++ src/main/java/org/example/Moneda.java
@@ -1,14 +1,30 @@
 package org.example;
 
-abstract class Moneda {
+abstract class Moneda implements Comparable<Moneda> {
     public Moneda() {
     }
-
-    ;
 
     public Moneda getSerie() {
         return this;
     }
 
     public abstract int getValor();
+
+    @Override
+    public int compareTo(Moneda moneda) {
+        if(this.getValor() > moneda.getValor()){
+            return 1;
+        }
+        else if (this.getValor() == moneda.getValor()){
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Esta moneda vale " + this.getValor() + "pesos";
+    }
 }
