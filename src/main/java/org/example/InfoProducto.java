64e888d264b6ae39c334f88cbc8066d40a28a2ca
Karantooo <ctomasalvarezn@gmail.com>
2023-10-19 17:12:00 -0300

    feat: Documentacion de ExpendedorTest e InfoProducto

--- src/main/java/org/example/InfoProducto.java
+++ src/main/java/org/example/InfoProducto.java
@@ -1,6 +1,13 @@
 package org.example;
 
+/**
+ * Enumeracion para representar las opciones de las maquinas como sus precios.
+ * @author Carlos Álvarez
+ */
 public enum InfoProducto {
+    /**
+     * Opciones de la máquina.
+     */
     COCA(1000),
     FANTA(900),
     SPRITE(800),
@@ -9,10 +16,18 @@ public enum InfoProducto {
 
     private int precio;
 
+    /**
+     * Constructor de la clase.
+     * @param precio precio para un determinado producto.
+     */
     private InfoProducto(int precio) {
         this.precio = precio;
     }
 
+    /**
+     * Se obtiene el precio del producto en particular.
+     * @return Devuelve el precio.
+     */
     public int getPrecio() {
         return precio;
     }
--- src/test/java/org/example/ExpendedorTest.java
+++ src/test/java/org/example/ExpendedorTest.java
@@ -5,51 +5,101 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Clase para testear expendedor.
+ * @author Benjamin Espinoza
+ */
 class ExpendedorTest {
     private Expendedor expendedor;
     private Producto producto;
     private Moneda moneda;
+
+    /**
+     * Creacion de expendedor para los test.
+     */
     @BeforeEach
     void getUp(){
         expendedor = new Expendedor(1);
     }
+
+    /**
+     * Verifica que compre Cocacola.
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void shouldReturnCocaCola()
+    void deberiaRetornarCocaCola()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         moneda = new Moneda1000();
         producto = expendedor.comprarProducto(moneda, InfoProducto.COCA);
         assertInstanceOf(CocaCola.class, producto);
     }
+
+    /**
+     * Verifica que compre sprite.
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void shouldReturnSprite()
+    void deberiaRetornarSprite()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         moneda = new Moneda1000();
         producto = expendedor.comprarProducto(moneda, InfoProducto.SPRITE);
         assertInstanceOf(Sprite.class, producto);
     }
+
+    /**
+     * Verifica que compre fanta.
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void shouldReturnFanta()
+    void deberiaRetornarFanta()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         moneda = new Moneda1000();
         producto = expendedor.comprarProducto(moneda, InfoProducto.FANTA);
         assertInstanceOf(Fanta.class, producto);
     }
+
+    /**
+     * Verifica que compre snickers.
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void shouldReturnSnickers()
+    void deberiaRetornarSnickers()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         moneda = new Moneda1000();
         producto = expendedor.comprarProducto(moneda, InfoProducto.SNICKERS);
         assertInstanceOf(Snickers.class, producto);
     }
+
+    /**
+     * Verifica que compre super 8.
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void shouldReturnSuper8()
+    void deberiaRetornarSuper8()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         moneda = new Moneda1000();
         producto = expendedor.comprarProducto(moneda, InfoProducto.SUPER8);
         assertInstanceOf(Super8.class, producto);
     }
+
+    /**
+     * Se verifica que se lance la excepcion NoHayProductoException
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void expendedorShouldThrowNoHayProductoException()
+    void expendedorThrowNoHayProductoException()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         moneda = new Moneda1000();
         producto = expendedor.comprarProducto(moneda,InfoProducto.COCA);
@@ -58,16 +108,30 @@ class ExpendedorTest {
             producto = expendedor.comprarProducto(moneda,InfoProducto.COCA);
         });
     }
+
+    /**
+     * Se verifica que se lance la excepcion PagoInsuficienteException
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void expendedorShouldThrowPagoInsuficienteException()
+    void expendedorThrowPagoInsuficienteException()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException{
         moneda = new Moneda500();
         assertThrows(PagoInsuficienteException.class, () ->{
             producto = expendedor.comprarProducto(moneda,InfoProducto.COCA);
         });
     }
+
+    /**
+     * Se verifica que se lance la excepcion PagoIncorrectoException
+     * @throws NoHayProductoException
+     * @throws PagoInsuficienteException
+     * @throws PagoIncorrectoException
+     */
     @Test
-    void expendedorShouldThrowPagoIncorrectoException()
+    void expendedorThrowPagoIncorrectoException()
             throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException{
         moneda = null;
         assertThrows(PagoIncorrectoException.class, () ->{
