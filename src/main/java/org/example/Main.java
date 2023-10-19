e3d4a9f08f337028ef343b2ed66425cf2d2a9e58
Benjamin Espinoza <beespinoza2022@inf.udec.cl>
2023-10-19 17:19:28 -0300

    feat: se agregaron las pruebas de funcionamiento en el main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,156 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Moneda moneda = new Moneda1000();
+        Expendedor expendedor1 = new Expendedor(1);
+        Expendedor expendedor2 = new Expendedor(1);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("comprador1: ");
+        Comprador comprador1;
+        try {
+            comprador1 = new Comprador(moneda, InfoProducto.COCA, expendedor1);
+            System.out.println(comprador1.queConsumiste());
+            System.out.println(comprador1.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("\ncomprador2: ");
+        Comprador comprador2;
+        moneda = new Moneda1000();
+        try {
+            comprador2 = new Comprador(moneda, InfoProducto.SPRITE, expendedor1);
+            System.out.println(comprador2.queConsumiste());
+            System.out.println(comprador2.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador3: ");
+        Comprador comprador3;
+        try {
+            comprador3 = new Comprador(moneda, InfoProducto.FANTA, expendedor1);
+            System.out.println(comprador3.queConsumiste());
+            System.out.println(comprador3.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador4: ");
+        Comprador comprador4;
+        try {
+            comprador4 = new Comprador(moneda, InfoProducto.SNICKERS, expendedor1);
+            System.out.println(comprador4.queConsumiste());
+            System.out.println(comprador4.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador5: ");
+        Comprador comprador5;
+        try {
+            comprador5 = new Comprador(moneda, InfoProducto.SUPER8, expendedor1);
+            System.out.println(comprador5.queConsumiste());
+            System.out.println(comprador5.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador6: ");
+        Comprador comprador6;
+        moneda = new Moneda1000();
+        try {
+            comprador6 = new Comprador(moneda, InfoProducto.COCA, expendedor1);
+            System.out.println(comprador6.queConsumiste());
+            System.out.println(comprador6.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador7: ");
+        Comprador comprador7;
+        moneda = new Moneda100();
+        try {
+            comprador7 = new Comprador(moneda, InfoProducto.COCA, expendedor1);
+            System.out.println(comprador7.queConsumiste());
+            System.out.println(comprador7.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador8:");
+        Comprador comprador8;
+        moneda = null;
+        try {
+            comprador8 = new Comprador(moneda, InfoProducto.COCA, expendedor1);
+            System.out.println(comprador8.queConsumiste());
+            System.out.println(comprador8.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador9: ");
+        Comprador comprador9;
+        moneda = new Moneda1000();
+        try {
+            comprador9 = new Comprador(moneda, InfoProducto.COCA, expendedor2);
+            System.out.println(comprador9.queConsumiste());
+            System.out.println(comprador9.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
+        }
+
+        System.out.println("\ncomprador10: ");
+        Comprador comprador10;
+        moneda = new Moneda1000();
+        try {
+            comprador10 = new Comprador(moneda, InfoProducto.SPRITE, expendedor2);
+            System.out.println(comprador10.queConsumiste());
+            System.out.println(comprador10.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No ingreso una opcion valida o no quedan productos en el expendedor");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("El precio del producto escogido es mayor al valor de su moneda");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Moneda invalida");
         }
     }
 }
