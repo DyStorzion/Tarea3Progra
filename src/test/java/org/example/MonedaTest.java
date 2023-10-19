15acfc6c2a67fccfb9a6c635c3b41aaba7d73a94
Benjamin Espinoza <beespinoza2022@inf.udec.cl>
2023-10-18 21:16:30 -0300

    feat: se agregan los test unitarios.

---
+++ src/test/java/org/example/MonedaTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonedaTest {
+
+    @org.junit.jupiter.api.Test
+    void getSerie() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getValor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+}
