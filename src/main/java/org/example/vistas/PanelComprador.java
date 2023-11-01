9527bc5990151a8c88300c72ee4fc42d46e6ecf6
Karantooo <ctomasalvarezn@gmail.com>
2023-11-01 20:27:10 -0300

    feat: Cambios en las ventanas y se añade boton para la compra pero sin ninguna logica con ello

--- src/main/java/org/example/vistas/PanelComprador.java
+++ src/main/java/org/example/vistas/PanelComprador.java
@@ -6,9 +6,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelComprador extends JPanel {
-    Comprador comprador;
+    private Comprador comprador;
+    JButton crearCompradorBoton;
+    JLabel descripcionCrearComprador;
+
     public PanelComprador(){
         this.setBackground(Color.RED);
+        this.setLayout(null);
+
+        crearCompradorBoton = new JButton();
+        crearCompradorBoton.setBounds(700, 650, 50, 50);
+
+        descripcionCrearComprador = new JLabel("Presione el boton para realizar una compra.");
+        descripcionCrearComprador.setBounds(450, 650, 275, 50);
+        descripcionCrearComprador.setOpaque(true);
+        descripcionCrearComprador.setBackground(Color.GRAY);
+
+
+        this.add(crearCompradorBoton);
+        this.add(descripcionCrearComprador);
     }
 
 
--- src/main/java/org/example/vistas/Ventana.java
+++ src/main/java/org/example/vistas/Ventana.java
@@ -6,9 +6,14 @@ public class Ventana extends JFrame {
     PanelPrincipal panelPrincipal;
     public Ventana(){
         this.panelPrincipal = new PanelPrincipal();
-        this.setSize(1000, 800);
+        this.setSize(1600, 800);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         getContentPane().add(panelPrincipal);
+        this.setTitle("Maquina expendedora");
+
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
         this.setVisible(true);
     }
 
