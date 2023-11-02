bb464013b377d8b1cec319525920b264c250333d
Karantooo <ctomasalvarezn@gmail.com>
2023-11-02 00:34:23 -0300

    feat: se añade implementacion de panel para deposito para monedas

--- src/main/java/org/example/vistas/PanelDepositoMoneda.java
+++ src/main/java/org/example/vistas/PanelDepositoMoneda.java
@@ -1,6 +1,32 @@
 package org.example.vistas;
 
+import org.example.modelos.Deposito;
+import org.example.modelos.Moneda;
+
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class PanelDepositoMoneda extends JPanel {
+    Deposito<Moneda> deposito;
+    ArrayList<PanelMoneda> panelesMonedas;
+
+    public PanelDepositoMoneda(Deposito<Moneda> deposito) {
+        this.deposito = deposito;
+        setLayout(new GridLayout(1, deposito.size()));
+        panelesMonedas = new ArrayList<>();
+        for (int i = 0; i < deposito.size(); i++){
+            Moneda moneda = deposito.getElemento();
+            panelesMonedas.add(new PanelMoneda(moneda));
+            this.add(panelesMonedas.get(panelesMonedas.size() - 1));
+            deposito.addElemento(moneda);
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+    }
 }
+
