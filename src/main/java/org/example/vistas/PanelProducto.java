32502a9274192d63e1626cb6f7ff65553dfaca96
Karantooo <ctomasalvarezn@gmail.com>
2023-11-01 22:13:50 -0300

    feat: Se añade el cuerpo de panel producto

--- src/main/java/org/example/vistas/PanelProducto.java
+++ src/main/java/org/example/vistas/PanelProducto.java
@@ -1,6 +1,23 @@
 package org.example.vistas;
 
+import org.example.modelos.Producto;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelProducto extends JPanel {
+    private Producto producto;
+
+    public PanelProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        switch (producto.consumir()){
+            case "cocacola":
+                break;
+        }
+    }
 }
