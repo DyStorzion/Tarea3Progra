a4ebdeea5d428fd7b61466bf03aba6a1f8f8a7f7
Benjamin Espinoza <beespinoza2022@inf.udec.cl>
2023-11-01 22:27:49 -0300

    fix: se arreglo un bug en el metodo paint.

--- src/main/java/org/example/vistas/PanelProducto.java
+++ src/main/java/org/example/vistas/PanelProducto.java
@@ -7,9 +7,23 @@ import java.awt.*;
 
 public class PanelProducto extends JPanel {
     private Producto producto;
-
+    private Image logoCocaCola;
+    private Image logoSprite;
+    private Image logoFanta;
+    private Image logoSnickers;
+    private Image logoSuper8;
+    private int anchoImagen;
+    private int altoImagen;
     public PanelProducto(Producto producto) {
+        this.setBackground(Color.cyan);
         this.producto = producto;
+        anchoImagen = 80;
+        altoImagen = 80;
+        logoCocaCola = new ImageIcon("assets/ImagenCocaCola.png").getImage().getScaledInstance(anchoImagen,altoImagen,Image.SCALE_SMOOTH);
+        logoSprite = new ImageIcon("assets/ImagenSprite.png").getImage().getScaledInstance(anchoImagen,altoImagen,Image.SCALE_SMOOTH);
+        logoFanta = new ImageIcon("assets/ImagenFanta.png").getImage().getScaledInstance(anchoImagen,altoImagen,Image.SCALE_SMOOTH);
+        logoSnickers = new ImageIcon("assets/ImagenSnickers.png").getImage().getScaledInstance(anchoImagen,altoImagen,Image.SCALE_SMOOTH);
+        logoSuper8 = new ImageIcon("assets/ImagenSuper8.png").getImage().getScaledInstance(anchoImagen,altoImagen,Image.SCALE_SMOOTH);
     }
 
     @Override
@@ -17,6 +31,7 @@ public class PanelProducto extends JPanel {
         super.paint(g);
         switch (producto.consumir()){
             case "cocacola":
+                g.drawImage(logoCocaCola, 0, 0, this);
                 break;
         }
     }
