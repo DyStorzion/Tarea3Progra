83cbb4cfdf1a452ab56dba8e35f5ccf7eac2aa4f
Karantooo <ctomasalvarezn@gmail.com>
2023-11-09 20:26:46 -0300

    docs: Se añade la documentacion de la clase de panelPrincipal

--- src/main/java/org/example/vistas/PanelPrincipal.java
+++ src/main/java/org/example/vistas/PanelPrincipal.java
@@ -3,9 +3,17 @@ package org.example.vistas;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Clase para el panel principal del expendedor.
+ * Esta dividido en 2 paneles principales, 1 para el expendedor y otro para el comprador
+ */
 public class PanelPrincipal extends JPanel {
     PanelComprador panelComprador;
     PanelExpendedor panelExpendedor;
+
+    /**
+     * Se inicialia el panel principal y se divide en 2 para los 2 paneles.
+     */
     public PanelPrincipal(){
         this.panelExpendedor = new PanelExpendedor();
         this.panelComprador = new PanelComprador(panelExpendedor);
